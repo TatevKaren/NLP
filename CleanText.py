eb10d4791234d3715af8c7f4ab717aef7f31130d
TK_Aslanyan <76843403+TatevKaren@users.noreply.github.com>
2022-10-14 13:39:16 +0200

    Add files via upload

---
+++ CleanText.py
@@ -0,0 +1,15 @@
+'Cleaning the data'
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+import re
+import string
+def clean_text(self, text):
+    text = text.lower()
+    text = re.sub("\d+", " ", text)
+    text = re.sub("\W+", " ", text)
+    text = word_tokenize(text)
+    stopWords = stopwords.words("English")
+    text = [word for word in text if word not in stopWords]
+    text = [word for word in text if word not in string.punctuation]
+    return ' '.join(text)
